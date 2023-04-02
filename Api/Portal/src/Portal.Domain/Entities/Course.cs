a10eb23ddde8fa532774bad3c8ec4d1679cb6e65
Nguyễn Xuân Nhân <nguyenxuannhan407@gmail.com>
2023-04-02 19:40:20 +0700

    Delete Api directory

--- Api/Portal/src/Portal.Domain/Entities/Course.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Portal.Domain.Entities;
-
-public class Course
-{
-    public string? courseId { get; set; }
-    public string? courseName { get; set; }
-    public string? description { get; set; }
-    public List<Class>? classes { get; set; }
-}

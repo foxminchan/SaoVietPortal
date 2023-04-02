ee714869967003ad1795095c5f881a5480640d3b
Nguyễn Xuân Nhân <nguyenxuannhan407@gmail.com>
2023-04-02 19:39:13 +0700

    deconstruct project structer

--- api/portal/src/Portal.Domain/Entities/PaymentMethod.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Portal.Domain.Entities;
-
-public class PaymentMethod
-{
-    public int paymentMethodId { get; set; }
-    public string? paymentMethodName { get; set; }
-    public List<CourseRegistration>? courseRegistrations { get; set; }
-}
--- api/portal/src/Portal.Domain/Entities/Position.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Portal.Domain.Entities;
-
-public class Position
-{
-    public int positionId { get; set; }
-    public string? positionName { get; set; }
-    public List<Staff>? staffs { get; set; }
-}

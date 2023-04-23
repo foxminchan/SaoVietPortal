d7366035a14d8def0d2d737e4cd75a131461e74c
Nguyễn Xuân Nhân <nguyenxuannhan407@gmail.com>
2023-04-24 02:43:05 +0700

    fix cònlict

---
+++ api/portal/src/Portal.Api/Validations/PaymentMethodValidator.cs
@@ -0,0 +1,12 @@
+using FluentValidation;
+using Portal.Api.Models;
+
+namespace Portal.Api.Validations;
+
+public class PaymentMethodValidator : AbstractValidator<PaymentMethod>
+{
+    public PaymentMethodValidator()
+        => RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Payment method name is required")
+            .MaximumLength(50).WithMessage("Payment method name must not exceed 50 characters");
+}
